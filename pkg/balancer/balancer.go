434777f84322597933731e4f1fb0a90c24a74d47
KilianSen <kilian@senger.group>
2026-04-21 18:44:48 +0200

    Update CORS headers to include User-Agent, Accept-Encoding, and Accept-Language

--- pkg/balancer/balancer.go
+++ pkg/balancer/balancer.go
@@ -102,7 +102,7 @@ func (b *Balancer) CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin, Accept, X-Node-Id, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Origin, Accept, X-Node-Id, X-Requested-With, User-Agent, Accept-Encoding, Accept-Language")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
 			return
